f47ccd7afd8a1e390a8b463657427bf98c2d2053
McCray <tubach75@hotmail.com>
2021-09-11 17:39:09 -0400

    commit tree 8b435e7c56033e9f6f309436dca606f084f63139

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,31 @@
-// TODO: Create action creators as defined in tests
+
+
+const addQuote = (quote) => {
+    return { 
+      type: 'ADD_QUOTE',
+      quote: Object.assign({}, quote, { votes: 0 })
+    };
+  };
+
+  const removeQuote = (quoteId) => {
+    return { 
+      type: 'REMOVE_QUOTE',
+      quoteId
+    };
+  };
+
+  const upvoteQuote = (quoteId) => {
+    return { 
+      type: 'UPVOTE_QUOUTE',
+      quoteId
+    };
+  };
+
+  const downvoteQuote = (quoteId) => {
+    return { 
+      type: 'DOWNVOTE_QUOUTE',
+      quoteId
+    };
+  };
+
+export default {addQuote, removeQuote, upvoteQuote, downvoteQuote}
